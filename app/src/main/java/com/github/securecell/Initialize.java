496af4d9331ac9649d89457103c2146e37d2765e
Clint Mourlevat <clint.mourlevat@gmail.com>
2015-02-21 03:24:37 +0100

    removing comments

--- app/src/main/java/com/github/securecell/Initialize.java
+++ app/src/main/java/com/github/securecell/Initialize.java
@@ -63,25 +63,9 @@ public class Initialize extends Application
 
                     String Response = ReplayRequest(Request);
                     out.write(Response);
-                    
-                    /*out.write("HTTP/1.1 200 OK\r\n");
-                    out.write("Server: Squid\r\n");
-                    out.write("Via: 1.0 perdu.com\r\n");
-                    out.write("X-Forwarded-For: 89.45.123.255\r\n");
-                    out.write("Content-Type: text/html\r\n");
-                    out.write("Content-Length: 2\r\n");
-                    out.write("\r\n");
-                    out.write("ok");*/
 
                     out.close();
                     in.close();
-
-                    /*PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    //Toast.makeText(getApplicationContext(), in.readLine(), Toast.LENGTH_LONG).show();
-                    Log.i("ds", in.readLine());
-                    out.write("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 13\r\nServer: Squid 1.3\r\nConnection: close\r\n\r\nhello world");
-                    out.close();*/
                 }
                 catch (IOException e)
                 {
